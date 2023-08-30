4070dcd7f12b7f3262ea4f8082f40fbf12704988
Huuthang98tn <tranhuuthang98fpttnt@gmail.com>
2023-08-30 17:52:33 +0700

    build Ios

--- android/app/src/main/java/com/erpviet/MainActivity.java
+++ android/app/src/main/java/com/erpviet/MainActivity.java
@@ -1,4 +1,4 @@
-package com.erpviet;
+package com.erpvietizisolotion;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/erpviet/MainApplication.java
+++ android/app/src/main/java/com/erpviet/MainApplication.java
@@ -1,4 +1,4 @@
-package com.erpviet;
+package com.erpvietizisolotion;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
